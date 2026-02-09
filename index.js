84b62e1d8f096122417b67450328415f898a6bf0
Sriji <ksriji06@gmail.com>
2026-02-09 23:53:29 +0530

    p..

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ await connectDB();
 
 const app = express();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
