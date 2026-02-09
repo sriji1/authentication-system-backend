3d925d40d239c0f3a9f5f19d70680e5f7dfbf1e1
Sriji <ksriji06@gmail.com>
2026-02-09 23:57:25 +0530

    p..

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+// importing routes
+import userRoutes from './routes/user.js'
+
+
 dotenv.config();
 await connectDB();
 
 const app = express();
+
+//using routes
+app.use("/api/v1", userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
